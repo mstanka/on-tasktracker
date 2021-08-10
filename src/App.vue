29b12540820077efa5ff566bd06df803d0c2c93c
mstanka <mice78.mm@gmail.com>
2021-08-10 15:09:01 +0200

    add task form

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
-<template></template>
+<template>
+  <section class="mx-auto mt-7 py-10 w-96 border bg-red-900">
+    <h1 class="text-yellow-300 text-center text-2xl uppercase pb-4">
+      Task Time Tracker
+    </h1>
+    <task-form></task-form>
+  </section>
+</template>
 
-<script></script>
+<script>
+import TaskForm from './components/TaskForm.vue';
 
-<style></style>
+export default {
+  components: {
+    TaskForm,
+  },
+};
+</script>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <form
+    @submit.prevent="submitForm"
+    class="bg-yellow-300 text-red-900 p-6 w w-80 mx-auto uppercase rounded-sm"
+  >
+    <div class="my-2">
+      <label for="name">Task Name</label>
+      <input type="text" id="name" v-model.trim="name" class="w-full" />
+    </div>
+    <div class="my-2">
+      <label for="type">Task Type</label>
+      <select name="type" id="type" v-model="type" class="w-full">
+        <option value="Write code">Write code</option>
+        <option value="Code Review">Code Review</option>
+        <option value="Meeting">Meeting</option>
+      </select>
+    </div>
+    <div class="my-2">
+      <label for="date">Date</label>
+      <input type="date" id="date" v-model="date" class="w-full" />
+    </div>
+    <div class="my-2">
+      <label for="from">From</label>
+      <input type="time" id="from" v-model="from" class="w-full" />
+    </div>
+    <div class="my-2">
+      <label for="to">To</label>
+      <input type="time" id="to" v-model="to" class="w-full" />
+    </div>
+    <div class="my-4">
+      <button
+        class="
+          bg-red-900
+          text-yellow-300
+          uppercase
+          border border-red-900
+          py-1
+          w-full
+          hover:text-red-900 hover:bg-yellow-300
+          transition
+          duration-500
+          ease-in-out
+        "
+      >
+        Submit Task
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '',
+      type: '',
+      date: null,
+      from: null,
+      to: null,
+    };
+  },
+  methods: {
+    submitForm() {
+      const formData = {
+        name: this.name,
+        type: this.type,
+        date: this.date,
+        from: this.from,
+        to: this.to
+      }
+      console.log(formData);      
+    },
+  },
+};
+</script>
